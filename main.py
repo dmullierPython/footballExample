e90f94af522ebd64ff8c6413506396a978afdfb6
Duncan Mullier <D.Mullier@leedsbeckett.ac.uk>
2022-11-20 15:57:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# Football Example
+
+if __name__ == '__main__':
+    matches = []        # blank list to store valid matches
+    line = ""
+    f = open("matches.txt")
+    print("Reading file.")
+    for line in f:      # loop for every line in  the file
+        splitList = line.split(" ")  # split into 4 element list
+        if len(splitList) != 4:
+            print("error with match data :", line, end="")
+            continue    # if an invalid line just go on and get the next line
+        try:            # see if we can convert the third and fourth items into numbers
+            splitList[2] = int(splitList[2])
+            splitList[3] = int(splitList[3])
+        except ValueError:  # if we can convert into numbers then it is an invalid ficture
+            print("invalid score ", line, end="")
+            continue    # if an invalid line just go on and get the next line
+        matches.append(splitList)
+
+    # now we have a list called "matches" which has valid matches
+    # <home team : string> <awayTeam : string> <homeScore: int> <awayScore : int>
+    highestScore = 0
+    highestScoreTeam = ""
+    print()
+    print("R E S U L T S")
+    for match in matches:
+        homeTeam = match[0]
+        awayTeam = match[1]
+        homeScore = match[2]
+        awayScore = match[3]
+        print(homeTeam, " ", homeScore, " : ", awayScore, " ", awayTeam)
+        if homeScore > highestScore:
+            highestScoreTeam = homeTeam
+            highestScore = homeScore
+        elif awayScore > highestScore:
+            highestScoreTeam = awayTeam
+            highestScore = awayScore
+    print("Highest score was ", highestScore, " by ", highestScoreTeam)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
